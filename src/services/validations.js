cab49ed222e771c8cea9fb9650dc0334bc315d8d
maxence.vangrunderbeec <maxence.vangrunderbeec@student.vinci.be>
2023-01-20 10:59:06 +0100

    fix: fix lint errors

--- src/services/validations.js
+++ src/services/validations.js
@@ -5,9 +5,9 @@ const checkGamerTag = (gamerTag) => {
     if(!gamerTag.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/))return false;
     if(!gamerTag.match(/[0-9]/))return false;
     return true;
-}
+};
 
 const isEmpty = (label) => !label || label.length === 0;
 
-module.exports = { isEmpty, checkGamerTag }
+module.exports = { isEmpty, checkGamerTag };
 
