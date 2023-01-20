8161eba59e91c0e17628709122946bfef37c94dd
maxence.vangrunderbeec <maxence.vangrunderbeec@student.vinci.be>
2023-01-20 11:05:35 +0100

    fix: fix prettier errors

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,13 +1,13 @@
 // TODO: Add here the function to validate
 
 const checkGamerTag = (gamerTag) => {
-    if(gamerTag.length < 8)return false;
-    if(!gamerTag.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/))return false;
-    if(!gamerTag.match(/[0-9]/))return false;
+    if (gamerTag.length < 8) return false;
+    if (!gamerTag.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/))
+        return false;
+    if (!gamerTag.match(/[0-9]/)) return false;
     return true;
 };
 
 const isEmpty = (label) => !label || label.length === 0;
 
 module.exports = { isEmpty, checkGamerTag };
-
